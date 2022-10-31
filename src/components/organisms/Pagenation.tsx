1e75e8b54a880ba10fe85befbc9546fbc6fc0f9a
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-10-31 19:22:20 +0900

    feat: update pagenation component

--- src/components/organisms/Pagenation.tsx
+++ src/components/organisms/Pagenation.tsx
@@ -3,16 +3,29 @@ import styles from '@styles/components/organisms/Pagenation.module.scss';
 
 type Props = {
   page: number;
+  total: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function Pagenation({page}: Props) {
+export default function Pagenation({page, total, setPage}: Props) {
+  const handlePagination = (direction: string) => {
+    let offset = page * 31;
+    let results = total || 0;
+    if (direction === "prev" && page >= 2) {
+      setPage(page - 1);
+    }
+    if (direction === "next" && page > 0 && offset < results) {
+      setPage(page + 1);
+    }
+  };
+
   return (
     <div className={styles.page}>
       <div className={styles.page__inner}>
         <div className={styles.page__content}>
-          <div className={styles.page__prev}>Prev</div>
+          <div className={styles.page__prev} onClick={() => handlePagination("prev")}>Prev</div>
           <p className={styles.page__now}>Page {page}</p>
-          <div className={styles.page__next}>Next</div>
+          <div className={styles.page__next} onClick={() => handlePagination("next")}>Next</div>
         </div>
       </div>
     </div>
