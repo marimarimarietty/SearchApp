2cd2f5cdbab7af60384604c73f0e71341aa0d7e9
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-10-31 16:26:49 +0900

    feat: update style of result component

--- src/components/organisms/SearchResult.tsx
+++ src/components/organisms/SearchResult.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import styles from '@styles/components/organisms/SearchResult.module.scss';
+import { GoStar as StarIcon, GoRepoForked as ForkIcon } from "react-icons/go";
 
 type Props = {
   data: any;
@@ -10,9 +11,25 @@ export default function SearchResult({data}: Props) {
   return (
     <div className={styles.result}>
       <div className={styles.result__inner}>
+        <div className={styles.result__total}>{data?.totalCount} results</div>
       {data?.items.map((elm:any, i:number) => {
         return (
-          <div key={i}>{elm.full_name}</div>
+          <div key={i} className={styles.item}>
+            <h3 className={styles.item__name}>{elm.full_name}</h3>
+            {elm.description && <p className={styles.item__desc}>{elm.description}</p>}
+            <div className={styles.item__detail}>
+              <div className={styles.item__star}>
+                <StarIcon />
+                <p className={styles.item__starText}>{elm.stargazers_count || 0}</p>
+              </div>
+              <div className={styles.item__fork}>
+                <ForkIcon />
+                <p className={styles.item__forkText}>{elm.forks_count || 0}</p>
+              </div>
+              {elm.language  && <div className={styles.item__language}>{elm.language}</div>}
+              {elm.updatedAt && <div className={styles.item__updated}>`Updated: {elm.updatedAt}`</div>}
+            </div>
+          </div>
         )
       })}
       </div>
